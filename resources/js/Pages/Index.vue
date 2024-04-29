61f9a96252854a264a1a8536f4d8ebbd97e09bf1
Kengketa <kengketa@gmail.com>
2024-04-29 14:03:32 +0700

    feat: add search filter

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -5,6 +5,13 @@
                 <h1 class="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-gray-700">เอกสารประกอบการสอน</h1>
                 <h1 class="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-blue-600">(PowerPoint)</h1>
             </div>
+            <div class="w-full px-4 md:px-6 lg:px-10 xl:px-16">
+                <label class="form-control w-full max-w-xs">
+                    <input ref="searchInputRef" v-model="search" class="input input-bordered w-full max-w-xs"
+                           placeholder="ค้นหา..."
+                           type="text"/>
+                </label>
+            </div>
             <div
                 class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-4 md:px-6 lg:px-10 xl:px-16 mt-4 lg:mt-10">
                 <div v-for="(subject,index) in subjectData" :key="subject.id">
@@ -15,7 +22,7 @@
                 <div class="join">
                     <Link v-for="(pagination,index) in pagination.links" :key="index"
                           :class="pagination.active ?'btn-active':''"
-                          :href="pagination.url"
+                          :href="pagination.url ?? '#'"
                           class="join-item btn">
                         {{ pagination.label }}
                     </Link>
@@ -30,6 +37,7 @@ import Layout from "@/Pages/Layout/Layout.vue";
 import TeachingMaterialCard from "@/Pages/Components/TeachingMaterialCard.vue";
 import axios from 'axios';
 import {Link} from "@inertiajs/vue3";
+import {router} from "@inertiajs/vue3";
 
 export default {
     name: "Index",
@@ -43,18 +51,34 @@ export default {
     data() {
         return {
             subjectData: null,
-            pagination: null
+            pagination: null,
+            search: new URLSearchParams(window.location.search).get('search') ?? null,
+            debouce: null
         };
     },
     mounted() {
         this.subjectData = this.subjects.data
         this.pagination = this.subjects.meta.pagination;
+        this.$refs.searchInputRef.focus();
     },
-
-    methods: {},
-
-    watch: {},
-
+    methods: {
+        async submitSearch() {
+            const url = this.route('index', {
+                search: this.search
+            })
+            await router.visit(url, {
+                only: ['subjects'],
+            })
+        }
+    },
+    watch: {
+        search() {
+            clearTimeout(this.debounce)
+            this.debounce = setTimeout(() => {
+                this.submitSearch();
+            }, 500);
+        }
+    },
     computed: {}
 };
 </script>
